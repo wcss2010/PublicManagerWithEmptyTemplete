d558782269435b3dfc5e83c6f8535b99fe312f78
wcss <wcss2020@gmail.com>
2020-03-27 14:39:57 +0800

    ...

--- Code/PublicManager/PublicManager/Modules/BaseDBImporter.cs
+++ Code/PublicManager/PublicManager/Modules/BaseDBImporter.cs
@@ -126,7 +126,7 @@ namespace PublicManager.Modules
         /// <param name="val"></param>
         /// <param name="defaultString"></param>
         /// <returns></returns>
-        protected virtual T getValueWithDefault<T>(T val, T defaultVal) where T : Object
+        protected virtual T getValueWithDefault<T>(T val, T defaultVal)
         {
             return val != null ? (T)Convert.ChangeType(val.ToString(), typeof(T)) : defaultVal;
         }
